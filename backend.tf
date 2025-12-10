d87b86900fda0820c3b57c0aeb4840628e8a7928
tariq <tariq@meetdeal.fr>
2025-12-10 11:08:55 +0100

    feat: change backend region

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "ct-s3-state-backend"
     key          = "infra-terraform"
-    region       = "eu-north-1"
+    region       = "eu-west-3"
     use_lockfile = true
   }
 }
