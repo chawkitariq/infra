4a1c27a07b2566f64183bb3ac8637c10f43c55ec
chawkitariq <chawkitariq@icloud.com>
2025-12-11 11:13:07 +0100

    feat: simplification

--- main.tf
+++ main.tf
@@ -15,14 +15,3 @@ module "eks" {
   source     = "./modules/eks"
   subnet_ids = module.vpc.private_subnet_ids
 }
-
-module "lbc" {
-  source                 = "./modules/aws-lbc"
-  cluster_name           = module.eks.cluster_name
-  region                 = var.region
-  vpc_id                 = var.vpc_id
-  enable_alb_gateway_api = true
-  enable_nlb_gateway_api = true
-
-  depends_on = [ module.eks ]
-}
--- providers.tf
+++ providers.tf
@@ -1,21 +1,3 @@
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
-}
-
 provider "aws" {
   region = var.region
-}
-
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
-provider "helm" {
-  kubernetes = {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-  }
 }
