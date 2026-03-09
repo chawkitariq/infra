7833bf75d333b40a218cc5d2c345122761750039
chawkitariq <chawkitariq@icloud.com>
2026-03-09 23:25:50 +0100

    setup outputs

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = var.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = module.vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  value       = module.vpc.private_subnet_ids
+}
